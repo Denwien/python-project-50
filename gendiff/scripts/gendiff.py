9bfc5c56ce4519e43f4dccc3ab7a393231195938
Denwien <denisgrigoriev1973@gmail.com>
2025-10-04 15:18:06 +0200

    Add file reading and parsing for gendiff CLI

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,30 +1,39 @@
 import argparse
+from pathlib import Path
+import json
+
+
+def read_file(file_path: str) -> dict:
+    
+    path = Path(file_path)
+    with path.open("r", encoding="utf-8") as f:
+        return json.load(f)
+
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.\n",
-        formatter_class=argparse.RawTextHelpFormatter,
-        epilog="options::\n  -h, --help            show this help message and exit\n  -f FORMAT, --format FORMAT\n                        set format of output"
+        description="Compares two configuration files and shows a difference.",
+        formatter_class=argparse.RawTextHelpFormatter
     )
-
-    # Опции
-    parser.add_argument(
-        "-f", "--format",
-        default="plain",
-        help=argparse.SUPPRESS  
-    )
-
-    
     parser.add_argument("first_file", help="Path to the first configuration file")
     parser.add_argument("second_file", help="Path to the second configuration file")
 
     args = parser.parse_args()
 
-    print(f"Comparing {args.first_file} with {args.second_file}")
-    print(f"Output format: {args.format}")
+    
+    file1_data = read_file(args.first_file)
+    file2_data = read_file(args.second_file)
+
+    print("File 1 data:", file1_data)
+    print("File 2 data:", file2_data)
+
 
 if __name__ == "__main__":
     main()
+
+
+
+
 
 
 
