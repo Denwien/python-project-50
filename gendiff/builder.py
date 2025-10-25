c55f6dde0cbd1a087168fd4c39059bc1ff50fada
Denwien <denisgrigoriev1973@gmail.com>
2025-10-25 15:00:48 +0200

    Refactor: move build_diff to builder.py, remove old scripts/gendiff.py

---
+++ gendiff/builder.py
@@ -0,0 +1,31 @@
+def build_diff(data1, data2):
+    """Рекурсивно строит diff между двумя словарями."""
+    diff_lines = []
+    all_keys = sorted(set(data1.keys()) | set(data2.keys()))
+
+    for key in all_keys:
+        val1 = data1.get(key)
+        val2 = data2.get(key)
+
+        if key not in data1:
+            diff_lines.append({"name": key, "action": "added", "value": val2, "children": []})
+        elif key not in data2:
+            diff_lines.append({"name": key, "action": "deleted", "value": val1, "children": []})
+        elif isinstance(val1, dict) and isinstance(val2, dict):
+            diff_lines.append({
+                "name": key,
+                "action": "nested",
+                "children": build_diff(val1, val2)
+            })
+        elif val1 == val2:
+            diff_lines.append({"name": key, "action": "unchanged", "value": val1, "children": []})
+        else:
+            diff_lines.append({"name": key, "action": "deleted", "value": val1, "children": []})
+            diff_lines.append({"name": key, "action": "added", "value": val2, "children": []})
+    return diff_lines
+
+
+
+
+
+
