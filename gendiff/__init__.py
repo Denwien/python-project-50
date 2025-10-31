ae2067414f19749eec435e5e01abd7e3f7ef8347
Denwien <denisgrigoriev1973@gmail.com>
2025-10-31 20:15:03 +0100

    Добавлен форматер plain и тесты для него

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,6 +1,7 @@
 from gendiff.parser import load_file
 from gendiff.builder import build_diff
 from gendiff.formaters.stylish import format_diff_stylish
+from gendiff.formaters.plain import format_diff_plain  # добавили plain
 
 def generate_diff(file_path1: str, file_path2: str, format_name: str = "stylish") -> str:
     data1 = load_file(file_path1)
@@ -10,4 +11,7 @@ def generate_diff(file_path1: str, file_path2: str, format_name: str = "stylish"
 
     if format_name == "stylish":
         return format_diff_stylish(diff_tree)
+    elif format_name == "plain":
+        return format_diff_plain(diff_tree)
     return diff_tree
+
---
+++ tests/test_plain.py
@@ -0,0 +1,15 @@
+import pytest
+from gendiff import generate_diff
+
+def test_plain_format():
+    file1 = "tests/test_data/file1.json"
+    file2 = "tests/test_data/file2.json"
+    expected_file = "tests/test_data/expected_output_plain.txt"
+
+    result = generate_diff(file1, file2, format_name='plain')
+
+    with open(expected_file, 'r') as f:
+        expected = f.read().strip()
+
+    assert result.strip() == expected
+
