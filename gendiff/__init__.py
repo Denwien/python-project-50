82b4a8b3e8ea5b0f3503f2dac0b752ff506ed654
Denwien <denisgrigoriev1973@gmail.com>
2025-11-04 14:47:40 +0100

    Fix generate_diff to accept format_name argument

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -2,9 +2,8 @@ from gendiff.formaters.format_identifier import format_identifier
 from gendiff.scripts.find_diff import find_diff
 from gendiff.parser import parse_data_from_file
 
-
-def generate_diff(file_path1, file_path2, formatter='stylish'):
+def generate_diff(file_path1, file_path2, format_name='stylish'):
     first_file = parse_data_from_file(file_path1)
     second_file = parse_data_from_file(file_path2)
     diff = find_diff(first_file, second_file)
-    return format_identifier(diff, formatter)
+    return format_identifier(diff, format_name)
