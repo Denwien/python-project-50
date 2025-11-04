583c8cbfea9d8323bad0c550a788e481a09dfdbd
Denwien <denisgrigoriev1973@gmail.com>
2025-11-04 17:52:28 +0100

    Fix plain formatter output

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,28 +1,5 @@
-def format_diff_plain(diff, path=''):
-    lines = []
-
-    for item in diff:
-        name = item['name']
-        action = item['action']
-        property_path = f"{path}.{name}" if path else name
-
-        if action == 'nested':
-            lines.extend(format_diff_plain(item['children'], property_path))
-        elif action == 'added':
-            value = stringify(item['value'])
-            lines.append(f"Property '{property_path}' was added with value: {value}")
-        elif action == 'deleted':
-            lines.append(f"Property '{property_path}' was removed")
-        elif action == 'changed':
-            old = stringify(item['old_value'])
-            new = stringify(item['new_value'])
-            lines.append(f"Property '{property_path}' was updated. From {old} to {new}")
-
-    return '\n'.join(lines)
-
-
-def stringify(value):
-    if isinstance(value, dict):
+def format_value(value):
+    if isinstance(value, (dict, list)):
         return '[complex value]'
     elif value is None:
         return 'null'
@@ -30,4 +7,44 @@ def stringify(value):
         return str(value).lower()
     elif isinstance(value, str):
         return f"'{value}'"
-    return str(value)
+    else:
+        return str(value)
+
+
+def make_plain_item(item, path=''):
+    key = item.get('name')
+    action = item.get('action')
+    new_value = format_value(item.get('new_value'))
+    old_value = format_value(item.get('old_value'))
+    current_path = f"{path}.{key}" if path else key
+
+    ADD = ' was added with value: '
+    REMOVE = ' was removed'
+    UPD = ' was updated. From '
+    UPD2 = ' to '
+    PROP = 'Property '
+
+    if action == 'added':
+        return f"{PROP}'{current_path}'{ADD}{new_value}"
+    if action == 'deleted':
+        return f"{PROP}'{current_path}'{REMOVE}"
+    if action == 'modified':
+        return f"{PROP}'{current_path}'{UPD}{old_value}{UPD2}{new_value}"
+    if action == 'nested':
+        children = item.get('children')
+        return make_plain_diff(children, current_path)
+    return None
+
+
+def make_plain_diff(diff, path=''):
+    result = []
+    for item in diff:
+        formatted_item = make_plain_item(item, path)
+        if formatted_item is not None:
+            result.append(formatted_item)
+
+    return '\n'.join(result)
+
+
+def format_diff_plain(diff):
+    return make_plain_diff(diff)
