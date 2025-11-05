a905ea750a189cd86bcc0f60550c349f0d30bbc7
Denwien <denisgrigoriev1973@gmail.com>
2025-11-05 15:01:31 +0100

    fix: исправлена функция format_diff_json

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,26 +1,21 @@
 import json
 
-def format_json(diff_tree):
-    """
-    Форматирует дерево изменений в JSON.
-    """
+def format_diff_json(diff_tree):
     def transform(node):
         action = node.get('action')
         if action == 'nested':
-            return {node['name']: [transform(child) for child in node['children']]}
-        elif action == 'added':
-            return {node['name']: node['value']}
-        elif action == 'deleted':
-            return {node['name']: node['old_value']}
-        elif action == 'modified':
+            return {node['name']: {k: v for d in [transform(child) for child in node['children']] for k, v in d.items()}}
+        if action == 'added':
+            return {node['name']: node.get('new_value', node.get('value'))}
+        if action == 'deleted':
+            return {node['name']: node.get('old_value')}
+        if action == 'modified':
             return {node['name']: {'old_value': node['old_value'], 'new_value': node['new_value']}}
-        elif action == 'unchanged':
+        if action == 'unchanged':
             return {node['name']: node['value']}
-        return node
+        return {}
 
-    # Merge all transformed nodes into a single dict
     result = {}
     for node in diff_tree:
         result.update(transform(node))
-    
     return json.dumps(result, indent=2)
